ce1b8f95327870e7f69248e2313e28dd1d7972e3
Jamie Chu <huanjamierose@gmail.com>
2017-12-28 16:24:49 -0500

    extra clarification for associated value types

--- EnumDemo/ViewController.swift
+++ EnumDemo/ViewController.swift
@@ -12,14 +12,22 @@ class ViewController: UIViewController {
 
     /*  Enumeration Syntax:
      
-     -to assign associated type of String to your Champion.Akali case
-     -to declare multiple cases can appear in same line, they must be of the same associated type. These are all Int types.
+     -you can assign "associated value of type" for your enum case.
+     --Champion.Kalista is an Int, CHampion.Caitlyn is a Double, Akali is a String, and Sona is a tuple of (Int, String, String, String)
+     -multiple cases can appear in same line, separated by a comma
+
      -syntax for referencing the enum.
+     -syntax for assigning the enumType via associated value
      
      -enums have an alternative to associated value types -> raw value types
      -rawChampion has raw value type of String with default(implicit) values of its property name.
      -you can override the default raw values like so
 
+     ->>Implicitly Assigned raw values
+     -enums of a default value will default to a starting value of 0 and increment each subsequent entry
+     -NOTE: if you assign a case with an already-implicitly-assigned value, Swift will not compile
+     
+     
      -tempChampion is inferred to be of type Champion so we can reassign its value without prefix
 
      -you can match functions to enums with a switch Statement.
@@ -27,8 +35,11 @@ class ViewController: UIViewController {
     
     enum Champion{
         case Akali(String)
-        case Kalista(Int), Vayne, Caitlyn, Khazix, Lee_Sin, Darius
-        case Sona, Janna
+        case Kalista(Int), Vayne, Caitlyn(Double), Khazix, Lee_Sin, Darius
+        case Sona(Int, String, String, String)
+        case Janna
+        
+        static let allValues:[Any] = [Akali, Kalista, Vayne, Caitlyn, Khazix, Lee_Sin, Darius, Sona, Janna]
     }
 
     enum RawChampionString: String{
@@ -38,8 +49,12 @@ class ViewController: UIViewController {
     }
     
     enum RawChampionInt:Int{
-        case Zac, Jax, Tahm_Kench
-        case Alistar = 99
+        case Zac, Jax, Tahm_Kench, Miss_Fortune, Sejuani
+        case Alistar = 99, Vladimir, Leona
+    }
+    
+    func printAllChampion(){
+        let _ = Champion.allValues.map{print($0)}
     }
     
     
@@ -59,17 +74,20 @@ class ViewController: UIViewController {
     
     func rawChampionString(for input: RawChampionString){
         switch input{
-        case .Ezreal:
-            print("case 1: \(input.rawValue)")
         case .Shen:
-            print("case 2: \(input.rawValue)")
+            print(".\(input) has custom rawValue of: \"\(input.rawValue)\"")
         default:
-            print ("default: \(input.rawValue)" )
+            print(".\(input) has default rawValue of: \"\(input.rawValue)\"")
         }
     }
 
     func rawChampionInt(for input: RawChampionInt){
-        
+        switch input{
+        case .Zac, .Jax, .Tahm_Kench, .Miss_Fortune, .Sejuani:
+            print(".\(input) has default rawValue of: \(input.rawValue)")
+        default:
+            print(".\(input) has non-default rawValue of: \(input.rawValue)")
+        }
         
     }
 
@@ -81,21 +99,36 @@ class ViewController: UIViewController {
         var tempChampion = Champion.Kalista(3)
         print(tempChampion)
         
-        tempChampion = .Janna
+        tempChampion = .Sona(3, "String1", "String2", "lastStringOfTuple")
+        let tempChampion1 = Champion.Sona(4, "DifferentSona", "2", "last")
         print(tempChampion)
-        
+        print(tempChampion1)
 
-        print("---------------------\nchampionCalls:\n---------------------")
+        print("---------------------\nprintAllChampion Call:\n---------------------")
+        printAllChampion()
+
+        print("---------------------\nchampionPosition Call:\n---------------------")
         championPositions(for: .Vayne)
         championPositions(for: .Janna)
         championPositions(for: .Darius)
 
-        print("---------------------\nrawChampionStringCalls:\n---------------------")
+        print("---------------------\nrawChampionString Call:\n---------------------")
         rawChampionString(for: .Ezreal)
         rawChampionString(for: .Shen)
         rawChampionString(for: .Ornn)
         
+        print("---------------------\nrawChampionInt Call:\n---------------------")
+        rawChampionInt(for: .Zac)
+        rawChampionInt(for: .Jax)
+        rawChampionInt(for: .Tahm_Kench)
+        rawChampionInt(for: .Miss_Fortune)
+        rawChampionInt(for: .Sejuani)
+        rawChampionInt(for: .Alistar)
+        rawChampionInt(for: .Vladimir)
+        rawChampionInt(for: .Leona)
         
+
+
     }
     
 }
